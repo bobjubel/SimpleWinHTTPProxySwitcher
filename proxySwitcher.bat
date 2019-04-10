27b036398c384c2075d4d8e948b1a40c35fe8773
Bob Jubel <buckpasser@yandex.ru>
2019-04-10 11:15:04 +0200

    Create proxySwitcher.bat

---
+++ proxySwitcher.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+CLS
+ECHO 1.WinHTTP Proxy ON
+ECHO 2.WinHTTP Proxy OFF
+ECHO 3.WinHTTP Proxy Status
+ECHO 4.Close BAT and reset Proxy
+ECHO.
+
+:REPEAT
+CHOICE /C 1234 /M "What do you want to do?: "
+
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 4 GOTO END
+IF ERRORLEVEL 3 GOTO ProxySTATUS
+IF ERRORLEVEL 2 GOTO ProxyOFF
+IF ERRORLEVEL 1 GOTO ProxyON
+
+:ProxyON
+netsh winhttp set proxy https://192.168.8.50:3128
+GOTO REPEAT
+
+:ProxyOFF
+netsh winhttp reset proxy
+GOTO REPEAT
+
+:ProxySTATUS
+netsh winhttp show proxy
+GOTO REPEAT
+
+:END
+netsh winhttp reset proxy
